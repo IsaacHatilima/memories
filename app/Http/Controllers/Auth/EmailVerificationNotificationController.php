eb49e38c71de92c0cbb4b6855800ca79cbdb5c7c
Isaac Hatilima <isaachatilima@gmail.com>
2024-10-04 21:22:44 +0200

    Added defer to EmailVerificationNotificationController

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\defer;
 
 class EmailVerificationNotificationController extends Controller
 {
@@ -17,7 +18,7 @@ class EmailVerificationNotificationController extends Controller
             return redirect()->intended(route('dashboard', absolute: false));
         }
 
-        $request->user()->sendEmailVerificationNotification();
+        defer(fn () =>$request->user()->sendEmailVerificationNotification());
 
         return back()->with('status', 'verification-link-sent');
     }
