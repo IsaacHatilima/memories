aeb3bae256176f8c6a4124f047e0740fae133a20
Isaac Hatilima <isaachatilima@gmail.com>
2024-10-05 19:17:37 +0200

    Updated interface

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,10 +1,20 @@
-import { Config } from 'ziggy-js';
+import {Config} from 'ziggy-js';
 
 export interface User {
     id: number;
     name: string;
     email: string;
     email_verified_at?: string;
+    two_factor_secret?: string;
+    two_factor_confirmed_at?: string;
+    profile?: Profile;
+}
+
+export interface Profile {
+    first_name: string;
+    last_name: string;
+    date_of_birth: string;
+    gender: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
